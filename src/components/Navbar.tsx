1f37f5f81ef320c170c8c93be8a848038ff85b6a
Advait <76993204+777advait@users.noreply.github.com>
2024-09-02 18:47:18 +0530

    update navbar logic

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import clsx from "clsx";
+import { useLocation } from "react-router-dom";
+
+const links = [
+  { href: "/", text: "about" },
+  { href: "/writings", text: "writings" },
+  { href: "/projects", text: "projects" },
+];
+
+export default function Navbar() {
+  const currentUrl = window.location.href;
+
+  const pathname = currentUrl.split("/")[3];
+  console.log(pathname);
+
+  return (
+    <header className="border-b py-2">
+      <nav className="flex items-center justify-between px-2.5">
+        <ul className="flex items-center gap-4">
+          {links.map((link) => (
+            <li key={link.href}>
+              <a
+                href={link.href}
+                className={clsx([
+                  "text-sm underline-offset-2 decoration-dashed lg:text-base",
+                  {
+                    underline: "/" + pathname === link.href,
+                    "text-muted-foreground": "/" + pathname === link.href,
+                  },
+                ])}
+              >
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          <li>
+            <a
+              className={clsx([
+                "text-sm underline-offset-2 decoration-dashed lg:text-base",
+                {
+                  underline: pathname === "/chat",
+                  "text-muted-foreground": pathname === "/chat",
+                },
+              ])}
+              href="/chat"
+            >
+              chat
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
