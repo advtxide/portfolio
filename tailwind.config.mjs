24ea7434ede268ae39a4738f4d9f98331b3d7343
Advait <76993204+777advait@users.noreply.github.com>
2024-09-04 09:10:54 +0530

    update color scheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -33,7 +33,7 @@ export default {
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
+					foreground: 'hsla(var(--foreground), 0.8)'
 				},
 				accent: {
 					DEFAULT: 'hsl(var(--accent))',
