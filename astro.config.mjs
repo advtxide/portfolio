7066f199d7d0e657186034cf8c727c1fe9bb260a
777advait <76993204+777advait@users.noreply.github.com>
2025-08-22 22:03:56 +0530

    feat!: website overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
-import sanity from '@sanity/astro';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://astro-dev.tech",
-  output: 'server',
-  prefetch: {
-    prefetchAll: true,
-  },
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    react(),
-    sanity({
-      projectId: "toj47gf1",
-      dataset: "production",
-      useCdn: false,
-      studioBasePath: "/admin",
-    }),
-    sitemap()
-  ],
-  adapter: vercel({
-    maxDuration: 30,
-    webAnalytics: {
-      enabled: true
-    }
-  })
-});
+export default defineConfig({});
