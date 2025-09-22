40a451be0a4da235b2115b813ab91993b0b500ac
777advait <76993204+777advait@users.noreply.github.com>
2025-09-22 20:36:22 +0530

    wip(revamp): landing page done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,34 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
-import sanity from '@sanity/astro';
-import sitemap from '@astrojs/sitemap';
+<<<<<<< HEAD
+import tailwindcss from "@tailwindcss/vite"
+import { loadEnv } from "vite"
+
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
+
+const { ASTRO_ADAPTER } = loadEnv(
+  process.env.MODE ?? "development",
+  process.cwd(),
+  "",
+)
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-dev.tech",
-  output: 'server',
-  prefetch: {
-    prefetchAll: true,
+  output: "server",
+  prefetch: { prefetchAll: true },
+  vite: {
+    plugins: [tailwindcss()],
   },
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    react(),
-    sanity({
-      projectId: "toj47gf1",
-      dataset: "production",
-      useCdn: false,
-      studioBasePath: "/admin",
-    }),
-    sitemap()
-  ],
-  adapter: vercel({
-    maxDuration: 30,
-    webAnalytics: {
-      enabled: true
-    }
-  })
+  integrations: [react()],
+  adapter: ASTRO_ADAPTER === "vercel" ? vercel() : node({ mode: "standalone" })
 });
+=======
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind()],
+});
+>>>>>>> 3c4fef19676f465d9e9caae117942315350457ca
---
+++ src/components/common/spotify.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { MusicIcon } from "lucide-react";
+
+interface SpotifyData {
+  title: string;
+  artist: string;
+}
+
+export function MarqueeText(props: { text: string }) {
+  const textLength = props.text.length;
+  const animationDuration = `${textLength / 2}s`; // Adjust the divisor for desired speed
+
+  return (
+    <div className="text-muted-foreground flex items-center overflow-hidden text-sm font-medium">
+      <span>
+        <MusicIcon className="mr-1 h-4 w-4" />
+      </span>
+      <div className="marquee-container max-w-full overflow-hidden whitespace-nowrap">
+        <div
+          className="marquee-content inline-block"
+          style={{ animationDuration: animationDuration }}
+        >
+          <span>{props.text}</span>
+          <span>{props.text}</span>
+          <span>{props.text}</span>
+          <span>{props.text}</span>
+          <span>{props.text}</span>
+          <span>{props.text}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Spotify() {
+  const [spotifyData, setSpotifyData] = useState<SpotifyData | null>(null);
+
+  useEffect(() => {
+    const fetchSpotifyData = async () => {
+      try {
+        const response = await fetch("/api/spotify");
+        const data = await response.json();
+        setSpotifyData(data.currentlyPlaying);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchSpotifyData();
+    const interval = setInterval(fetchSpotifyData, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!spotifyData) {
+    return <MarqueeText text="Not listening to music rn..." />;
+  }
+
+  const { title, artist } = spotifyData;
+  const marqueeText = `${title} - ${artist}`;
+
+  return <MarqueeText text={marqueeText} />;
+}
